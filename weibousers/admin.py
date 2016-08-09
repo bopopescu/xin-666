82ca79f27a180e2026dce2b373823b1a94ff45a1
Eden Park <edenpark.dev@gmail.com>
2016-08-10 11:41:55 +1200

    add post_count field to weibouser on admin page

--- weibousers/admin.py
+++ weibousers/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
+from django.db import models
 from .models import WeiboUser
+from location.models import Post
+
 # Register your models here.
+
+class PostInline(admin.TabularInline):
+    model = Post
+    fields = [
+    	'place', 'weibo_id', 'created', 'text', 'weibo_img', 'category'
+    ]
+    readonly_fields = (
+    	'place', 'weibo_id', 'created', 'text', 'weibo_img', 'category'
+    )
+    extra = 0
+    can_delete = False
 
 class WeiboUserAdmin(admin.ModelAdmin):
     list_filter = ['gender',]
-    list_display = ('weibo_name', 'weibo_id', 'gender', 'province', 'city', 'location')
+    list_display = ('weibo_name', 'weibo_id', 'gender', 'province', 'city', 'location', 'post_count')
+    inlines = (PostInline,)
 
+    def get_queryset(self, request):
+    # def queryset(self, request): # For Django <1.6
+        qs = super(WeiboUserAdmin, self).get_queryset(request)
+        # qs = super(CustomerAdmin, self).queryset(request) # For Django <1.6
+        qs = qs.annotate(models.Count('post'))
+        return qs
+
+    def post_count(self, obj):
+        return obj.post__count
+    post_count.admin_order_field = 'post__count'
 admin.site.register(WeiboUser, WeiboUserAdmin)
--- weibousers/models.py
+++ weibousers/models.py
@@ -8,16 +8,16 @@ GENDER_CHOICES = (
 )
 
 class WeiboUser(models.Model):
-	weibo_id = models.BigIntegerField(null=True, blank=True, unique=True)
-	weibo_name = models.CharField('weibo name', max_length=40, blank=True, null=True)
-	gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-	province = models.PositiveSmallIntegerField(null=True, blank=True)
-	city = models.PositiveSmallIntegerField(null=True, blank=True)
-	location = models.CharField(max_length=120, null=True, blank=True)
+    weibo_id = models.BigIntegerField(null=True, blank=True, unique=True)
+    weibo_name = models.CharField('weibo name', max_length=40, blank=True, null=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    province = models.PositiveSmallIntegerField(null=True, blank=True)
+    city = models.PositiveSmallIntegerField(null=True, blank=True)
+    location = models.CharField(max_length=120, null=True, blank=True)
 
-	class Meta:
-	    verbose_name = 'weibo user'
-	    verbose_name_plural = 'weibo users'
+    class Meta:
+        verbose_name = 'weibo user'
+        verbose_name_plural = 'weibo users'
 
-	def __unicode__(self):
-	    return '%s' % (self.weibo_id,)
+    def __unicode__(self):
+        return '%s' % (self.weibo_id,)
