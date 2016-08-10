4848b04263600b7860521b866461eacbff6b83c1
Eden Park <edenpark.dev@gmail.com>
2016-08-10 12:33:41 +1200

    add visited_place_count to sort

--- weibousers/admin.py
+++ weibousers/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.db import models
+from django.db.models import Count
 from .models import WeiboUser
 from location.models import Post
-
 # Register your models here.
 
 class PostInline(admin.TabularInline):
@@ -18,17 +18,26 @@ class PostInline(admin.TabularInline):
 
 class WeiboUserAdmin(admin.ModelAdmin):
     list_filter = ['gender',]
-    list_display = ('weibo_name', 'weibo_id', 'gender', 'province', 'city', 'location', 'post_count')
+    list_display = (
+    	'weibo_name', 'weibo_id', 'gender', 'province', 'city', 
+    	'location', 'post_count', 'place_count',
+    )
     inlines = (PostInline,)
 
     def get_queryset(self, request):
     # def queryset(self, request): # For Django <1.6
         qs = super(WeiboUserAdmin, self).get_queryset(request)
         # qs = super(CustomerAdmin, self).queryset(request) # For Django <1.6
-        qs = qs.annotate(models.Count('post'))
+        qs = qs.annotate(Count('post'))\
+        	.annotate(Count('post__place', distinct=True))
         return qs
 
     def post_count(self, obj):
         return obj.post__count
     post_count.admin_order_field = 'post__count'
+
+    def place_count(self, obj):
+        return obj.post__place__count
+    place_count.admin_order_field = 'post__place__count'
+
 admin.site.register(WeiboUser, WeiboUserAdmin)
