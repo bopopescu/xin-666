4dbb50b043cef4242e07fab45b2cf847d4e23380
Eden Park <edenpark.dev@gmail.com>
2016-08-19 11:04:13 +0900

    add urls and create base.html and templates

--- location/urls.py
+++ location/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, url
-from .views import index, place_index
+from .views import index, place_index, categorise_post
+# from location import views
 
 urlpatterns = [
     url(r'^$', index, name = "index"),
-    url(r'^location/(?P<place_id>[0-9]+)/$', place_index, name = "place_index"),
+    url(r'^location/(?P<place_id>[0-9]+)/$', place_index, name="place_index"),
+    # url(r'^location/(?P<place_id>[0-9]+)categorise_post/categorise/new/$', new_post, name="new_post"),
+    url(r'^location/(?P<place_id>[0-9]+)/categorise(?:/(?P<post_id>[0-9]+))?/$', 
+    	categorise_post, name="categorise_post"),
 ]
--- location/views.py
+++ location/views.py
@@ -14,7 +14,11 @@
 
 from django.http import HttpResponse
 from .models import Place, Post
-from django.shortcuts import render
+from .forms import CategorisePostForm
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic.edit import UpdateView
+from django.core.urlresolvers import reverse
 
 def index(request):
 	cx = {}
@@ -32,9 +36,38 @@ def index(request):
 				
 	return render(request, "index.html", cx)
 
+
 def place_index(request, place_id):
     cx = {}
     place = Place.objects.get(id=place_id)
     cx['place'] = place
-    cx['posts'] = Post.objects.filter(place=place).order_by('created')[:10]
+    cx['posts'] = Post.objects.filter(place=place).order_by('-created')[:10]
     return render(request, "location/place_index.html", cx)
+
+
+def categorise_post(request, place_id, post_id=None):
+	place = Place.objects.get(id=place_id)
+	if post_id:
+		obj = get_object_or_404(Post, pk=post_id)
+		print 'has post id'
+	else:
+		obj = Post.objects.filter(place=place, category=None).order_by('created').first()
+		print 'haven\' post id'
+	form = CategorisePostForm(request.POST or None, instance=obj)
+	print request.method
+	if request.method == 'POST':
+		if form.is_valid():
+			print "form.is_valid"
+			form.save()
+		return redirect(reverse('location:categorise_post', kwargs={'place_id': place_id}))
+	print "not valid"
+	return render(request, 'location/categorise_post_form.html', 
+			{'form': form, 'obj': obj})
+
+
+@staff_member_required
+class CategorisePostView(UpdateView):
+    # form_class = CategorisePostForm
+    model = Post
+    fields = ('place', 'user', 'created', 'text', 'weibo_img', 'category',)
+    template_name_suffix = '_update_form'
