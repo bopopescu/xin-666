7170d973d8abda4f35988861a8dfbf5021319c52
EDEN PARK <edenp@techday.com>
2016-09-14 09:14:31 +1200

    changes on post model and add image_path()

--- location/models.py
+++ location/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from weibousers.models import WeiboUser
+import os
 
+def image_path(instance, filename):
+    return 'images/{0}/{1}/{2}'.format(instance.place.pk, instance.created.strftime("/%Y/%m/%d/"), filename)
 
 class Place(models.Model):
     name = models.CharField(db_index=True, max_length=120)
@@ -17,13 +20,16 @@ class Post(models.Model):
     place =  models.ForeignKey(Place)
     user = models.ForeignKey(WeiboUser)
     weibo_id = models.BigIntegerField(null=True, blank=True, unique=True)
-    created = models.DateField(blank=True, null=True)
+    created = models.DateTimeField(blank=True, null=True)
     text = models.TextField(blank=True, null=True)
-    weibo_img = models.CharField(max_length=150, blank=True, null=True)
+    weibo_img = models.URLField(max_length=150, blank=True, null=True)
+    weibo_thumb_img = models.URLField(max_length=150, blank=True, null=True)
     category = models.ForeignKey('categories.Category', blank=True, null=True)
+    image = models.ImageField(blank=True, null=True, upload_to=image_path)
 
     class Meta:
         ordering = ('created',)
 
     def __unicode__(self):
         return '%s' % (self.weibo_id,)
+
